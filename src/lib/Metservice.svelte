371e6005053a5433e244798ec96aca776c5e874a
Edward Wright (1000126272) <WRIGEG1@student.op.ac.nz>
2024-10-16 14:12:55 +1300

    josh is Happy because the met componet has been created

---
+++ src/lib/Metservice.svelte
@@ -0,0 +1,22 @@
+<script>
+  const location = "Dunedin";
+
+  const temp = 11.9;
+  const tempHigh = 12.1;
+  const tempLow = 5.3;
+  const windspeed = "Light winds 11km/h SW";
+  const rain = "0.0mm";
+</script>
+
+<h2>Met Serivce</h2>
+<ul>
+  <li>Location: {location}</li>
+  <li>{temp}</li>
+  <li>{tempHigh}</li>
+  <li>{tempLow}</li>
+  <li>{windspeed}</li>
+  <li>{rain}</li>
+</ul>
+
+<style>
+</style>
