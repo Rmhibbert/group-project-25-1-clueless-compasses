e464a02803c311ccf7454d8cf6466fa0c0911799
SamBat2005 <batcsg1@student.op.ac.nz>
2024-10-16 13:42:15 +1300

    Started work on the tides component

---
+++ src/lib/Tides.svelte
@@ -0,0 +1,29 @@
+<script>
+
+</script>
+
+<h1>Latest tide information for Dunedin</h1>
+<section>
+    <h2>Tide graph goes here...</h2>
+</section>
+
+<section>
+    <h2>Dunedin Tides timetable</h2>
+    <ul>
+        <li>Wed 16 Oct
+            <p>High and Low tides</p>
+        </li>
+        <li>
+            Wed 17 Oct
+            <p>High and Low tides</p>
+        </li>
+        <li>
+            Thu 18 Oct
+            <p>High and Low tides</p>
+        </li>
+    </ul>
+</section>
+
+<style>
+
+</style>
