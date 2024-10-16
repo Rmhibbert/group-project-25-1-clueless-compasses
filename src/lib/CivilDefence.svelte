fe2dd84fa9b7197131ae5a69b2502d34360bd3ec
Mustafa12315 <HABIM3@student.op.ac.nz>
2024-10-16 14:12:48 +1300

    Added the hard-coded text

--- src/lib/CivilDefence.svelte
+++ src/lib/CivilDefence.svelte
@@ -3,9 +3,14 @@
 </script>
 
 <section>
-    
+<h1>Alerts and Warnings</h1>
+    <h3>Dunedin Civil defense History</h3>
+    <ol>
+        <a href="/"><li>Alerts Today</li></a>
+        <a href="/"><li>Severe Weather Conditions</li></a>
+    </ol>
 </section>
 
 <style>
- /*styles for the Webpage*/
+    
 </style>
