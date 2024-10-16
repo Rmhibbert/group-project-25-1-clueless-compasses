8ec56ac4a3d1594063696da4b6b76e65f21a2aac
Edward Wright (1000126272) <WRIGEG1@student.op.ac.nz>
2024-10-16 14:36:11 +1300

    fixed up the wording and display

--- src/lib/Metservice.svelte
+++ src/lib/Metservice.svelte
@@ -9,14 +9,18 @@
 </script>
 
 <h2>Met Serivce</h2>
+<h3>Warning</h3>
+<h4>No Warnings at the moment</h4>
 <ul>
-  <li>Location: {location}</li>
-  <li>{temp}</li>
-  <li>{tempHigh}</li>
-  <li>{tempLow}</li>
-  <li>{windspeed}</li>
-  <li>{rain}</li>
+  <li><strong>Location: </strong>{location}</li>
+  <li><strong>Temperature: </strong>{temp}</li>
+  <li><li>Temperature (High): </li>{tempHigh}</li>
+  <li><strong>Temperature (Low): </strong>{tempLow}</li>
+  <li><strong>Windspeed: </strong>{windspeed}</li>
+  <li><strong>Rainfall: </strong>{rain}</li>
 </ul>
+
+
 
 <style>
 </style>
