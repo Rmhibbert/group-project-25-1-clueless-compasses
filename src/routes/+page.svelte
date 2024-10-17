7605dada21430e79ef43fd1fb5ca1555da53fb83
SamBat2005 <batcsg1@student.op.ac.nz>
2024-10-17 17:42:27 +1300

    Fixed typo in import URL for CD component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
   import Navigation from "$lib/Navigation.svelte";
   import Metservice from "$lib/Metservice.svelte";
-  import CivilDefence from "$/lib/CivilDefence.svelte";
+  import CivilDefence from "$lib/CivilDefence.svelte";
 </script>
 
 <Navigation />
