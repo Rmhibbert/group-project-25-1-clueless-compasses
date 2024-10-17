3d5f897d1c5009071204a1fcc5b766dc46baef45
SamBat2005 <batcsg1@student.op.ac.nz>
2024-10-18 11:39:45 +1300

    Test commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,17 +3,13 @@
   import Metservice from "$lib/Metservice.svelte";
   import CivilDefence from "$lib/CivilDefence.svelte";
   import Footer from "$lib/Footer.svelte";
-<<<<<<< HEAD
   import GeoNet from "$lib/GeoNet.svelte";
   import Tides from '$lib/Tides.svelte';
-=======
->>>>>>> 81d9e43bf6ff1286b28a0ce8a438b486df5e510b
 </script>
 <Navigation />
 
 <main>
-  <Metservice />
-  
+  <Metservice /> 
   <CivilDefence /> 
   <GeoNet />
   <Tides />
@@ -25,7 +21,6 @@
   :global(*) {
     font-family: sans-serif;
   }
-
   main{
     padding-top: 2em;
     display: flex;
