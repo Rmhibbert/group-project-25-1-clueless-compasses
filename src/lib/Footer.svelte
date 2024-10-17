c671b4331cbc5454361f5ef0b996edcccf2d6ed0
Joshua Smith <169119042+smitja21@users.noreply.github.com>
2024-10-17 20:03:59 +1300

    Created Basic Footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -2,8 +2,19 @@
 
 </script>
 
-<p>Footer</p>
+<footer>
+    <h2>Disaster Management System</h2>
+    <p>Your one stop shop for information during the time you need it most<p>
+    <p>&copy; 2024 Otago Polytech Studio 2 - The 5 Stooges</p>
+</footer>
 
 <style>
-
+    footer {
+        background-color: rgb(0, 0, 0);
+        color: white;
+        text-align: center;
+        margin-top: 3em;
+        padding: 1.5em 0 1.5em;
+        line-height: 1.5em;
+    }
 </style>
