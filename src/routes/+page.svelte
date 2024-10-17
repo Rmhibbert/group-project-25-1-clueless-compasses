0dc14272a2fc5e73aba6da449f12386eab3221f6
SamBat2005 <batcsg1@student.op.ac.nz>
2024-10-17 20:42:04 +1300

    Resolved local merge conflicts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,18 +4,27 @@
   import CivilDefence from "$lib/CivilDefence.svelte";
   import Footer from "$lib/Footer.Svelte";
 </script>
-
 <Navigation />
 
-<Metservice />
+<main>
+  <Metservice />
 
-<CivilDefence />
+  <CivilDefence />
 
-<Footer />
+  <!--More components go below-->
 
+</main>
 <style>
-        :global(*) {
-        font-family: sans-serif;
-    }
+  main{
+    padding-top: 2em;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 20px;
+  }
+  /*Global selection width for each component */
+  :global(section){
+    width: 350px;
+  }
 </style>
 
