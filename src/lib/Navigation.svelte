0a491bb205c16d2f13a70728c3892c57892ef1b0
Samuel Batchelor <batcsg1@student.op.ac.nz>
2024-10-23 17:07:33 +1300

    Implemented hamburger menu for when page width goes below 820px

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -1,5 +1,15 @@
 <script>
+    /*To ensure hamburger and dropdown-menu are not null*/
+    import { onMount } from 'svelte';
 
+    onMount(() => {
+    const hamburger = document.getElementById('hamburger');
+    const dropdownMenu = document.getElementById('dropdown-menu');
+
+    hamburger.addEventListener("click", (e) => {
+        dropdownMenu.style.display = dropdownMenu.style.display === 'flex' ? 'none' : 'flex';
+    });
+});
 </script>
 
 <header>    
@@ -13,11 +23,22 @@
             <li><a href="/">Hato Hone St John</a></li>
         </ul>
         <!--Hamburger Menu-->
-        
+        <ul>
+            <li>
+                <a id="hamburger" href="/">☰</a>
+            </li>
+        </ul>
+    </nav>
+    <nav id="dropdown-menu">
+        <ul >
+            <!-- Add items for the dropdown menu here -->
+            <li><a href="/">Home</a></li>
+            <li><a href="/">FENZ</a></li>
+            <li><a href="/">Police</a></li>
+            <li><a href="/">Hato Hone St John</a></li>
+        </ul>
     </nav>
 </header>
-
-
 
 <style>
     header{
@@ -25,40 +46,73 @@
         justify-content: center;
         align-items: center;
         box-shadow: 0 4px 4px -2px rgba(0, 0, 0, 0.1);
+        background-color: lightskyblue;   
     }
-    nav {
-        background-color: red;
+    h1 {
+        width: 25em;
+        font-size: 20px;
+        text-transform: uppercase;
+        text-decoration: underline;
+        letter-spacing: 4px;
+        
+        text-align: center;
+    }
+    ul li a {
+    text-decoration: none;
+    color:#333;
     }
     ul {
         display: flex;
-        justify-content: space-around;
+        justify-content: center;
         list-style: none;
         font-size: 1.2em;
-        /* Puts the title onto the same line */     
-        background-color: yellow;
+        /* Puts the title onto the same line */      
+        /*Centers list elements directly*/
+        padding: 1em; 
     }
     ul li{
         padding: 1em;
         border-radius: 10px;
-        margin-inline: 0.4em;
-        background-color: greenyellow;
+        margin: 10px;
+        background-color: rgb(39, 160, 230);
     }
-
-h1 {
-    width: 15em;
-}
-
-ul li a {
-    text-decoration: none;
-    color:#333;
-}
+    /*Hamburger menu list*/
+    ul:last-of-type{
+        display: none;
+    }
+    #dropdown-menu{
+        display: none;
+        background-color: rgb(46, 46, 46);
+        width: 600px;
+        /*Take the hamburger menu outside of the normal document flow*/
+        position: absolute;
+        top: 180px;    
+    }
+    #dropdown-menu ul{
+        flex-direction: column;
+        margin-left: 1em;
+        /*Remove default padding*/
+        padding: 0px;
+    }
+    #dropdown-menu ul li{
+        background-color: inherit;
+        
+        /*Remove default padding*/
+        padding: 1em;
+    }
+    #dropdown-menu ul li a{
+        color: white;
+    }
 
 @media (width <= 820px){
-    header{
-        background-color: red;    
-        flex-direction: column;    
+    /*First list on nav menu*/
+    ul:first-of-type{
+        display: none;
     }
-
+    /*Last list on nav menu for hamburger menu*/
+    ul:last-of-type{
+        display: flex;
+    }
 }
 
 </style>
