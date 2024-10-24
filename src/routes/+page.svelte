93edce83ecc7bddbb84e774a9dc51fcc24b89600
HugoSCode <smitht8@student.op.ac.nz>
2024-10-25 11:26:45 +1300

    Added some styling for the cards , a bit of box shadow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,6 +24,9 @@
   /*Global selection width for each component */
   :global(section){
     width: 350px;
-    border: 1px ridge #333;
+    box-shadow: rgba(0, 0, 0, 0.35) 0.2em 0.5em 1em;
+    border-radius: 0.5em;
+    padding: 2em;
   }
+
 </style>
