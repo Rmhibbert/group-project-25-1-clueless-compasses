b6876a9f919a530c56353366deb63d9046da35e8
Joshua Smith <169119042+smitja21@users.noreply.github.com>
2024-10-31 12:19:33 +1300

    Removed unused styles

--- src/lib/GeoNet.svelte
+++ src/lib/GeoNet.svelte
@@ -51,17 +51,4 @@
   </section>
 
 <style>
-  li {
-    list-style: none;
-    margin-left: 0;
-    border-radius: 0.2em;
-    box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
-    padding: 0.15em;
-    padding-left: 0.5em;
-
-  }
-  ul{
-    padding: 0;
-    margin: 0%;
-  }
 </style>
