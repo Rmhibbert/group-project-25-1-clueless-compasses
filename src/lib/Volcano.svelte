38c2daa2eafe642e381dedefef93dcceb5d45e94
Mustafa12315 <HABIM3@student.op.ac.nz>
2024-10-30 13:25:00 +1300

    Added the basic fetch for volcano

---
+++ src/lib/Volcano.svelte
@@ -0,0 +1,13 @@
+<script >
+    import { onMount } from 'svelte';
+
+//Blank Array
+let volcanos = [];
+
+onMount(async () => {
+  //Shows earthquakes with a 
+  const res = await fetch(`https://api.geonet.org.nz/quake?MMI=6`);
+  let data = await res.json();
+  volcanos = data.features;
+});
+</script>
