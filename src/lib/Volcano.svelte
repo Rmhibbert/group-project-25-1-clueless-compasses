17f0818a984d92253a82ea185ecbc51a151b4c4f
Mustafa12315 <HABIM3@student.op.ac.nz>
2024-10-30 13:38:58 +1300

    Added the basic displaying volcanos

--- src/lib/Volcano.svelte
+++ src/lib/Volcano.svelte
@@ -1,13 +1,27 @@
 <script >
     import { onMount } from 'svelte';
 
-//Blank Array
 let volcanos = [];
 
 onMount(async () => {
-  //Shows earthquakes with a 
-  const res = await fetch(`https://api.geonet.org.nz/quake?MMI=6`);
+  const res = await fetch(`https://api.geonet.org.nz/volcano/val`);
   let data = await res.json();
   volcanos = data.features;
+  console.log(volcanos);
 });
 </script>
+<section>
+<h1>Volcano </h1>
+{#each volcanos as volcano}
+    <h3>{volcano.properties.volcanoTitle}</h3>
+    <p>Activity: {volcano.properties.activity}</p>
+    <p>Level: {volcano.properties.level}</p>
+    <p>Hazards: {volcano.properties.hazards}</p>
+    <p>ACC: {volcano.properties.acc}</p>
+    {/each}
+</section>
+<style>
+    section{
+      overflow:scroll; height:30em;
+    }
+</style>
