6a4d31a2d019d6a942add627c7357553b4c86a08
Joshua Smith <169119042+smitja21@users.noreply.github.com>
2024-10-31 14:37:54 +1300

    Added arcgis map

---
+++ src/lib/Map.svelte
@@ -0,0 +1,28 @@
+<script>
+    import MapView from "@arcgis/core/views/MapView";
+    import "@arcgis/core/assets/esri/themes/light/main.css";
+  
+    const createMap = (domNode) => {
+      const view = new MapView({
+        container: domNode,
+        map: {
+          basemap: "streets-vector",
+        },
+        zoom: 15,
+        center: [-90.1928, 38.6226], // longitude, latitude
+      });
+    };
+  </script>
+  
+
+  <section>
+    <div class="view" use:createMap></div>
+  </section>
+
+  <style>
+    .view {
+      height: 400px;
+      width: 800px;
+    }
+  </style>
+  
