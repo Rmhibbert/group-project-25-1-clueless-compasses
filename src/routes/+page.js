f6c0515a46cded60130e4aa49639563e5da935dc
Joshua Smith <169119042+smitja21@users.noreply.github.com>
2024-10-31 14:38:03 +1300

    Work around to allow map to load

---
+++ src/routes/+page.js
@@ -0,0 +1,4 @@
+//Allows map to be loaded
+//Ref: https://stackoverflow.com/questions/76180960/svelte-vite-node-module-import-error-error-when-evaluating-ssr-module-failed
+export const ssr = false;
+export const csr = true;
