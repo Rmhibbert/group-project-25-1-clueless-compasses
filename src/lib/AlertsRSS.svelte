8006ee91ee64f2906c32dc26529b6346bb919b34
Mustafa12315 <HABIM3@student.op.ac.nz>
2024-11-01 11:58:54 +1300

    Added the proper styling for the rss feed

--- src/lib/AlertsRSS.svelte
+++ src/lib/AlertsRSS.svelte
@@ -1,19 +1,33 @@
 <script>
-  </script>
-  
-  <section>
-    <h1>Metservice Alerts</h1>
-    <ul>
-      <div id="widgetmain"><div id="rsswidget" style="height:500px;"><iframe src="https://www.rssfeedwidget.com/getrss.php?time=1730414224022&amp;x=https%3A%2F%2Falerts.metservice.com%2Fcap%2Frss&amp;w=200&amp;h=500&amp;bc=333333&amp;bw=1&amp;bgc=transparent&amp;m=20&amp;it=false&amp;t=(default)&amp;tc=333333&amp;ts=15&amp;tb=transparent&amp;il=true&amp;lc=000000&amp;ls=14&amp;lb=true&amp;id=true&amp;dc=333333&amp;ds=14&amp;idt=true&amp;dtc=284F2D&amp;dts=12" border="0" hspace="0" vspace="0" frameborder="no" marginwidth="0" marginheight="0" style="border:0; padding:0; margin:0; width:200px; height:500px;" id="rssOutput">Reading RSS Feed ...</iframe></div><div style="text-align:right;margin-bottom:0;border-top:1px solid #333333;" id="widgetbottom"><span style="font-size:70%"><a href="https://www.rssfeedwidget.com">rss feed widget</a>&nbsp;</span><br></div></div>
-    </ul>
-  </section>
+  // Your JavaScript logic here (if needed)
+</script>
 
-  <style>
-    #widgetmain {
-      text-align:center;
-      overflow-y:auto;
-      overflow-x:hidden;
-      width:200px;
-    }
-  </style>
-  
+<section>
+  <h1>Metservice Alerts</h1>
+  <div id="widgetmain">
+    <div id="rsswidget" style="height: 500px;">
+      <iframe 
+        src="https://www.rssfeedwidget.com/getrss.php?time=1730414224022&amp;x=https%3A%2F%2Falerts.metservice.com%2Fcap%2Frss&amp;w=200&amp;h=500&amp;bc=333333&amp;bw=1&amp;bgc=transparent&amp;m=20&amp;it=false&amp;t=(default)&amp;tc=333333&amp;ts=15&amp;tb=transparent&amp;il=true&amp;lc=000000&amp;ls=14&amp;lb=true&amp;id=true&amp;dc=333333&amp;ds=14&amp;idt=true&amp;dtc=284F2D&amp;dts=12" 
+        border="0" 
+        frameborder="no" 
+        style="border: 0; padding: 0; margin: 0; width: 340px; height: 500px;" 
+        id="rssOutput">
+        Reading RSS Feed ...
+      </iframe>
+    </div>
+    <div style="text-align: right; margin-top: 0; border-top: 1px solid #333333;" id="widgetbottom">
+      <span style="font-size: 70%">
+        <a href="https://www.rssfeedwidget.com">rss feed widget</a>&nbsp;
+      </span><br>
+    </div>
+  </div>
+</section>
+
+<style>
+  #widgetmain {
+    text-align: center;
+    overflow-y: auto;
+    overflow-x: hidden;
+    width: 100%;
+  }
+</style>
