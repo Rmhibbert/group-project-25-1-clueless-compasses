42c3b9326efd975f1d05072711fe6037be8acbc7
Your Name <smitht8@student.op.ac.nz>
2024-11-07 12:25:44 +1300

    adding a function to select different cities tides to look at. taking some code form the one i did in metservice

--- src/lib/Tides.svelte
+++ src/lib/Tides.svelte
@@ -3,6 +3,19 @@
     let tides = {};
     let values = [];
 
+    const cities = [
+    { name: "Auckland Harbour", lat: -36.8406, lon: 174.7650 },
+    { name: "Wellington Harbour", lat: -41.2834, lon: 174.7790 },
+    { name: "Lyttelton Harbour (Christchurch)", lat: -43.6028, lon: 172.7190 },
+    { name: "Tauranga Harbour", lat: -37.6555, lon: 176.1770 },
+    { name: "Napier Port", lat: -39.4784, lon: 176.9186 },
+    { name: "Nelson Harbour", lat: -41.2708, lon: 173.2830 },
+    { name: "Dunedin Harbour", lat: -45.8788, lon: 170.5080 },
+];
+
+let selectedCity=cities[6].name;
+
+
 onMount(async () => {
   const res = await fetch(`https://api.niwa.co.nz/tides/data?lat=-45.88584470756695&long=170.51294108229843&numberOfDays=2&apikey=UcV9ZQdL9GnLtHymRjPsFovznDzAc0xJ`);
   tides = await res.json();
@@ -13,6 +26,14 @@ const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit
 </script>
 
 <section>
+
+    <label for="city">Choose a city:</label>
+    <select id="city" bind:value={selectedCity} on:change={() => fetchWeather(selectedCity)}>
+      {#each cities as city}
+        <option value={city.name}>{city.name}</option>
+      {/each}
+    </select>
+
     <h1>Dunedin Harbour Tides</h1>
     <ul>
         <!-- <li><strong>Latitude: {tides.metadata?.latitude}</strong></li>
