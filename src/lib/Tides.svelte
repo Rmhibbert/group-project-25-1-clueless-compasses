7fc6b55878283aa4537ef36e62d51c486c9b3e6a
HugoSCode <smitht8@student.op.ac.nz>
2024-11-06 14:35:37 +1300

    Modified the card sepcify high and low tides

--- src/lib/Tides.svelte
+++ src/lib/Tides.svelte
@@ -2,31 +2,61 @@
     import { onMount } from 'svelte';
     let tides = {};
     let values = [];
-
-onMount(async () => {
-  const res = await fetch(`https://api.niwa.co.nz/tides/data?lat=-45.88584470756695&long=170.51294108229843&numberOfDays=2&apikey=UcV9ZQdL9GnLtHymRjPsFovznDzAc0xJ`);
-  tides = await res.json();
-  values = tides.values;
-});
-const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: false };
-
-</script>
-
-<section>
+    let tidePairs = [];
+  
+    onMount(async () => {
+      const res = await fetch(`https://api.niwa.co.nz/tides/data?lat=-45.88584470756695&long=170.51294108229843&numberOfDays=2&apikey=UcV9ZQdL9GnLtHymRjPsFovznDzAc0xJ`);
+      tides = await res.json();
+      values = tides.values;
+      getTides(); 
+    });
+  
+    function getTides() {
+      tidePairs = [];  
+      for (let i = 0; i < values.length; i += 2) {
+        tidePairs.push([values[i], values[i + 1]]);
+        console.log(tidePairs[i]);
+      }
+    }
+  
+    const options = {
+      year: 'numeric', 
+      month: 'long', 
+      day: 'numeric', 
+      hour: '2-digit', 
+      minute: '2-digit', 
+      hour12: false,
+      timeZone: 'Pacific/Auckland'
+    };
+  </script>
+  
+  <section>
     <h1>Dunedin Harbour Tides</h1>
     <ul>
-        <!-- <li><strong>Latitude: {tides.metadata?.latitude}</strong></li>
-        <li><strong>Longitude: {tides.metadata?.longitude} </strong></li> -->
-        <li><strong>Start: {new Date(tides.metadata?.start).toDateString()} </strong></li>
-        <li><strong>{tides.metadata?.days} day forecast</strong></li>
+      <li><strong>Start: {new Date(tides.metadata?.start).toDateString()}</strong></li>
+      <li><strong>{tides.metadata?.days} day forecast</strong></li>
     </ul>
+  
     <ul id="tide-list">
-        {#each values as value }
-          <li ><strong>{new Date(value.time).toLocaleString('en-NZ', options)}</strong></li>
-          <li class="value">Height: {value.value}m</li>
-        {/each}
+      {#each tidePairs as pair}
+        <li>
+          <div>
+            <strong>Low Tide: {new Date(pair[0].time).toLocaleString('en-NZ', options)}</strong>
+            <p>Height: {pair[0].value}m</p>
+          </div>
+          
+          <div>
+            <strong>High Tide: {new Date(pair[1].time).toLocaleString('en-NZ', options)}</strong>
+            <p>Height: {pair[1].value}m</p>
+          </div>
+        </li>
+      {/each}
     </ul>
-</section>
+  </section>
+  
+  
+  
+  
 
 <style>
     #tide-list{
