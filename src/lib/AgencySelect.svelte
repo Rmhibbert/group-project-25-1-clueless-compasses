ec2b156a990e79ef4ed687fcc8a6802de0601aa9
smitja21 <smitja21@student.op.ac.nz>
2025-03-03 16:25:21 +1300

    Styled Drop Down

--- src/lib/AgencySelect.svelte
+++ src/lib/AgencySelect.svelte
@@ -2,12 +2,32 @@
     import { selectedAgency } from "$lib/stores.js";
 </script>
 
-<select bind:value={$selectedAgency}>
-    <option value="All">All</option>
-    <option value="FENZ">FENZ</option>
-    <option value="GeoNet">GeoNet</option>
-    <option value="USAR">USAR</option>
+<select bind:value={$selectedAgency} class="dropdown">
+    <option value="All">🌍 All</option>
+    <option value="FENZ">🚒 FENZ</option>
+    <option value="GeoNet">🌋 GeoNet</option>
+    <option value="USAR">🦺 USAR</option>
 </select>
 
 <style>
+    .dropdown {
+        padding: 0.6em 1em 0.6em 1em;
+        font-family: inherit;
+        font-size: inherit;
+        font-weight: bolder;
+        background-color: #333;
+        color: white;
+        cursor: pointer;
+    }
+
+    :global(body.dark) .dropdown{
+        background-color: white;
+        color: #333;
+    }
+
+    /* Focus effect */
+    .dropdown:focus {
+        border-color: #007bff;
+        box-shadow: 0 0 5px rgba(0, 123, 255, 0.5);
+    }
 </style>
