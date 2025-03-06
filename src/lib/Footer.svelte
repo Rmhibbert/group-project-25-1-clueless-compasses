99784e3f5ff6fe9d77fd86bb813fefb063de75ad
Your Name <smitht8@student.op.ac.nz>
2025-03-07 09:56:30 +1300

    Footer at the bottom but lots of white space between it and the content

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -2,34 +2,47 @@
 
 </script>
 <body>
+    <main>
+
+    </main>
+
     <footer>
         <h2>Disaster Management System</h2>
-        <p>Your one stop shop for information during the time you need it most<p>
-        <p>&copy; 2025 Otago Polytechnic Studio 3 - The clueless compasses</p>
+        <p>Your one stop shop for information during the time you need it most</p>
+        <p>&copy; 2025 Otago Polytechnic Studio 3 - The Clueless Compasses</p>
     </footer>
 </body>
 
+
 <style>
 
-    body {
-  min-height: 100vh;
+body {
+  min-height: 100vh; 
   margin: 0;
-  display: grid;
-  grid-template-rows: auto 1fr auto;
+  display: flex;
+  flex-direction: column;
+}
+
+main {
+  flex: 1; 
+}
+
+footer {
+  box-shadow: rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, 
+              rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, 
+              rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, 
+              rgba(0, 0, 0, 0.06) 0px 2px 1px, 
+              rgba(0, 0, 0, 0.09) 0px 4px 2px, 
+              rgba(0, 0, 0, 0.09) 0px 8px 4px, 
+              rgba(0, 0, 0, 0.09) 0px 16px 8px, 
+              rgba(0, 0, 0, 0.09) 0px 32px 16px;
+  background: linear-gradient(48deg, rgba(0,0,0,1) 28%, rgba(116,116,116,1) 100%);
+  color: white;
+  text-align: center;
+  min-height: 50px;
+  width: 100%;
 }
 
 
-    footer {
-        box-shadow: rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;
-        background: rgb(0,0,0);
-        background: linear-gradient(48deg, rgba(0,0,0,1) 28%, rgba(116,116,116,1) 100%);
-        color: white;
-        text-align: center;
-        padding: 1.5em 0 1.5em;
-        min-height: 50px;
-        position: fixed;
-        width: 100%;
-        margin-top: auto;
-        bottom: 0;
-    }
+
 </style>
