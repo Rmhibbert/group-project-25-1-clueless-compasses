fa590ae8b3dab30eea83d2cb561995008a66fcde
Joshua Smith <169119042+smitja21@users.noreply.github.com>
2025-03-12 20:44:22 +1300

    fix: found missing styling (unsure if I removed it on mistake on was from upsteam)

--- src/lib/Volcano.svelte
+++ src/lib/Volcano.svelte
@@ -9,28 +9,36 @@
         volcanos = data.features;
     });
 
+    //Object of emojis for conversion from colour to emoji used by the getEmoji function
     const emojis = {
-        Green: "🟩",
-        Yellow: "🟨",
-        Red: "🟥"
+            Green: "🟩",
+            Yellow: "🟨",
+            Red: "🟥",
+            Orange:"🟧"
     };
 
+    //Sets a background and text colour of the acc displayed from api
     function getACCStyles(acc) {
         switch (acc) {
             case 'Green':
-                return 'text-white bg-green-600';
+                return { color: 'white', backgroundColor: 'green' };
             case 'Yellow':
-                return 'text-black bg-yellow-400'; // Adjusted for better contrast
+                return { color: 'black', backgroundColor: 'yellow' }; // Changed to black for contrast
             case 'Red':
-                return 'text-white bg-red-600';
+                return { color: 'white', backgroundColor: 'red' };
+            case 'Orange':
+            return { color: 'black', backgroundColor: 'Darkorange' }; // Changed to black for contrast
             default:
-                return 'text-white bg-gray-800';
+                return { color: 'white', backgroundColor: '#333' };
+            }
         }
-    }
 
+
+    //Using the emojis object to grab the emoji corresponding to the colour 
     function getEmoji(acc) {
         return emojis[acc] || "⚪"; // Default emoji for unknown acc
     }
+
 </script>
 
 <section>
