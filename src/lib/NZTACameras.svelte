1752072bf5a2838b8c187cb90d9579dbc74f3c22
Joshua Smith <169119042+smitja21@users.noreply.github.com>
2025-03-19 21:26:14 +1300

    feat: shows a message asking the user to select a camera

--- src/lib/NZTACameras.svelte
+++ src/lib/NZTACameras.svelte
@@ -7,26 +7,35 @@
     let selectedCameraId = null;
     let selectedCamera = null;
 
-  // Find the selected camera based on the ID
-  $: selectedCamera = otagoCameras.find(camera => camera.id === selectedCameraId);
+    // Find the selected camera based on the ID
+    $: selectedCamera = otagoCameras.find(
+        (camera) => camera.id === selectedCameraId,
+    );
 </script>
 
 <section>
     <h2 class="text-2xl font-bold mb-2">NZTA Traffic Camera</h2>
 
-    <select bind:value={selectedCameraId}
-    class="mb-2 w-fit p-1 mt-2 rounded bg-white text-gray-800 border cursor-pointer focus:outline-none focus:ring- focus:ring-blue-500">
+    <select
+        bind:value={selectedCameraId}
+        class="mb-2 w-fit p-1 mt-2 rounded bg-white text-gray-800 border cursor-pointer focus:outline-none focus:ring- focus:ring-blue-500"
+    >
         <option value="" disabled>Select a camera</option>
-        
+
         {#each otagoCameras as camera}
             <option value={camera.id}>{camera.name}</option>
         {/each}
     </select>
 
-        {#if selectedCamera}
+    {#if selectedCamera}
         <div>
-            <img src={selectedCamera.imageUrl} alt={selectedCamera.name || "Image Loading"} />
+            <img
+                src={selectedCamera.imageUrl}
+                alt={selectedCamera.name || "Image Loading"}
+            />
             <p>{selectedCamera.description}</p>
         </div>
+    {:else}
+        <p class=" text-tex font-bold">Please select a traffic camera from above</p>
     {/if}
 </section>
