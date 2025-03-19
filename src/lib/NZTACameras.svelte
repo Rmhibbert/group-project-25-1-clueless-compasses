227e7a046ed546bda3a04f9c368f4947f289a5a3
Joshua Smith <169119042+smitja21@users.noreply.github.com>
2025-03-19 21:11:35 +1300

    style: implemented dropdown menu stype

--- src/lib/NZTACameras.svelte
+++ src/lib/NZTACameras.svelte
@@ -14,8 +14,10 @@
 <section>
     <h2 class="text-2xl font-bold mb-2">NZTA Traffic Camera</h2>
 
-    <select bind:value={selectedCameraId}>
+    <select bind:value={selectedCameraId}
+    class="mb-2 w-fit p-1 mt-2 rounded bg-white text-gray-800 border cursor-pointer focus:outline-none focus:ring- focus:ring-blue-500">
         <option value="" disabled>Select a camera</option>
+        
         {#each otagoCameras as camera}
             <option value={camera.id}>{camera.name}</option>
         {/each}
