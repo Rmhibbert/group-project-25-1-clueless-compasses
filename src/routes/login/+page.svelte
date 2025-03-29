178748dd264fda37de10728eef6123fcdb73fc63
BEKEZA1@student.op.ac.nz <BEKEZA1@student.op.ac.nz>
2025-03-29 13:38:25 +1300

    made a very very basic page with the text boxes to type in

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,50 @@
+<script>
+
+  import { onMount } from "svelte";
+
+  let password = '';
+  let username = '';
+
+
+  function  enterData()
+  {
+
+  }
+
+  function clearData ()
+  {
+
+  }
+
+
+
+
+
+</script>
+
+<p>Username</p>
+<input
+  id="name"
+  type="text"
+  placeholder="Please enter your username"
+  bind:value={username}
+  />
+<p>Password</p>
+<input
+  id="password"
+  type="text"
+  placeholder="Please enter your password"
+  bind:value={password}
+/>
+<p>Phone Number</p>
+
+
+<button class="submit" on:click={enterData}>Click here to Submit your Data</button>
+
+
+<button class="clear" on:click={clearData}>Click here to clear your Data.</button>
+
+
+<style>
+
+</style>
