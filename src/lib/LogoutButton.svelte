6cc5f2fc60268f4cec2d4c166b44268f3e085ee0
ZandarB <BEKEZA1@student.op.ac.nz>
2025-04-07 16:26:58 +1200

    style: added a bit more button styleing

--- src/lib/LogoutButton.svelte
+++ src/lib/LogoutButton.svelte
@@ -1,4 +1,4 @@
 <button class="bg-gray-700 text-white py-2 px-4 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-300" 
-    on:click={THingyHere}>
+    on:click={ThingyHere}>
         Logout
 </button>
