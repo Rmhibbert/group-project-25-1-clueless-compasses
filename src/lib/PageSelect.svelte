dce73df0c119d7b9fd16c5e734b43f268642d86c
Your Name <smitht8@student.op.ac.nz>
2025-04-10 12:48:57 +1200

    Changed the page switching logic to work with Wills nav

---
+++ src/lib/PageSelect.svelte
@@ -0,0 +1,46 @@
+<script>
+	import { goto } from '$app/navigation';
+
+	function navigate(event) {
+		const page = event.target.value;
+		if (page === 'home') {
+			goto('/');
+		} else {
+			goto(`/${page}`);
+		}
+	}
+</script>
+
+<select on:change={navigate} class="dropdown">
+	<option value="home">Home</option>
+	<option value="login">Login</option>
+	<option value="hazard">Hazard Reporting</option>
+	<option value="contact">Contact Us</option>
+</select>
+
+
+
+<style>
+    .dropdown {
+        padding: 0.6em 1em 0.6em 1em;
+        font-family: inherit;
+        font-size: inherit;
+        font-weight: bolder;
+        background-color: #333;
+        color: white;
+        cursor: pointer;
+        outline: none; 
+        border-top: 2px solid #535353;
+        border-left: 2px solid #535353;
+        border-bottom: 2px solid black;
+        border-right: 2px solid black;
+    }
+    
+    :global(body.dark) .dropdown {
+        background-color: white;
+        color: #333;
+    }
+    </style>
+
+
+    
