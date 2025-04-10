153d2c7aca01f1a953481ee9333e8a75e93f02a9
ZandarB <BEKEZA1@student.op.ac.nz>
2025-04-11 10:11:43 +1200

    made logout button look similar to the other buttons on the nav bar

--- src/lib/LogoutButton.svelte
+++ src/lib/LogoutButton.svelte
@@ -1,4 +1,26 @@
-<button class="bg-gray-700 text-white py-2 px-4 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-300" 
+<button class=""
     on:click={ThingyHere}>
         Logout
 </button>
+
+<style>
+        button{
+        padding: 0.6em 1em 0.6em 1em;
+        font-family: inherit;
+        font-size: inherit;
+        font-weight: bolder;
+        background-color: #333;
+        color: white;
+        cursor: pointer;
+        border: black;
+        border-top: 2px solid #535353;
+        border-right: 2px solid black;
+        border-bottom: 2px solid black;
+        border-left: 2px solid #535353;
+        width: 10em;
+    }
+    :global(body.dark) button{
+        background-color: white;
+        color: #333;
+    }
+</style>
