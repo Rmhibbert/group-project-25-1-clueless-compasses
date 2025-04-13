af2045d9e4c543cf2fb9dd007187e86edb129531
Joshua Smith <smitja21@student.op.ac.nz>
2025-04-14 11:47:07 +1200

    style: improved styling and added emoji for colour status

--- src/lib/RSSEmergencyAlerts.svelte
+++ src/lib/RSSEmergencyAlerts.svelte
@@ -1,29 +1,38 @@
-<script lang="ts">
-    export let metserviceAlerts;
-    const alerts = metserviceAlerts.rss.channel.item ?? [];
+<script>
+    export let fullAlertDetails;
+    console.log(fullAlertDetails); // Check the prop value
+
+    //Object of emojis for conversion from colour to emoji used by the getEmoji function
+    const emojis = {
+        Green: "🟩",
+        Yellow: "🟨",
+        Orange: "🟧",
+        Red: "🟥",
+    };
+
+    function getEmoji(colorValue) {
+        return emojis[colorValue] || "⚪";
+    }
+
 </script>
 
 <section>
     <h1 class="text-2xl font-bold">(New) Metservice Warnings</h1>
 
-    {#if alerts.length > 0}
+    {#if fullAlertDetails && fullAlertDetails.length > 0}
         <ul class="space-y-4">
-            {#each alerts as alert}
+            {#each fullAlertDetails as alert}
                 <li class="border p-4 rounded-lg shadow">
-                    <h2 class="text-xl font-semibold">{alert.title}</h2>
-                    <p class="text-sm text-gray-600">{alert.pubDate}</p>
+                    <h2 class="text-xl font-semibold">
+                        {getEmoji(alert.parameter[1].value)}
+                        {alert.title}
+                    </h2>
+                    <p class="text-sm">{alert.area.areaDesc}</p>
                     <p class="mt-2">{alert.description}</p>
-                    <a
-                        class="text-blue-500 underline mt-2 inline-block"
-                        href={alert.link}
-                        target="_blank"
-                    >
-                        View Full Alert
-                    </a>
+                    <p class="text-sm text-gray-600">{alert.pubDate}</p>
                 </li>
             {/each}
         </ul>
-        <!-- If there are no current alerts displays a message -->
     {:else}
         <p>Loading alerts or none available.</p>
     {/if}
