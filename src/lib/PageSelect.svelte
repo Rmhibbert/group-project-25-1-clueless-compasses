063d7a1ca0e718c256ececa39248d2773b8322d9
WillAdamsonPolytech <adamwd1@student.op.ac.nz>
2025-04-23 15:43:33 +1200

    Feat(Navigation): Page select now works BUT WILL NEED THE REAL PAGES ADDED WHEN MERGED TO STAGING!! The login and home page are sorted, but because the other pages aren't done yet I can't add them.

--- src/lib/PageSelect.svelte
+++ src/lib/PageSelect.svelte
@@ -1,21 +1,22 @@
 <script>
     import { selectedPage } from "$lib/stores.js";
     import { goto } from '$app/navigation';
+    import { get } from 'svelte/store';
 
     function PageChange() {
-    const newPage = selectedPage;
-    goto(`/${newPage}`);
+    const route = get(selectedPage);
+    goto(`/${route}`);
   }
 </script>
 
+<!-- Binds menu value to a variable, then passes that variable to a function that converts it to a url -->
 <select bind:value={$selectedPage} 
 on:change={PageChange}
 class="px-4 py-[0.6em] font-bold bg-[#333] text-white cursor-pointer outline-none border-t-[2px] border-l-[2px] border-b-[2px] border-r-[2px] border-t-[#535353] border-l-[#535353] border-b-black border-r-black"
 >
-    <option value="Login">Login</option>
-    <option value="Home">Home</option>
-    <option value="Hazard">Harzard Reporting</option>
-    <option value="Contact">Contact Us</option>
+    <option value="login">Login</option>
+    <option value="">Home</option>
+    <option value="hazard">Harzard Reporting</option>
 </select>
 
 
