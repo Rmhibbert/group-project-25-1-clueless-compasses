3a5c967b98edaa1efc20d450666a29851ddd47b3
WillAdamsonPolytech <adamwd1@student.op.ac.nz>
2025-04-25 14:56:08 +1200

    Feat(incidents): Start styling incidents page - added ability to upload photos. Doesn't go anywhere as we don't have a functional backend yet

--- src/routes/incident/+page.svelte
+++ src/routes/incident/+page.svelte
@@ -1,24 +1,71 @@
 <script>
+    let avatar, fileinput;
 
+    const onFileSelected =(e)=>{
+        let image = e.target.files[0];
+            let reader = new FileReader();
+            reader.readAsDataURL(image);
+            reader.onload = e => {
+                avatar = e.target.result
+            };
+    }
 </script>
 
+<!-- Header -->
+<h1 class="text-3xl font-bold text-gray-800 border-b-2 border-gray-300 p-1 text-center">
+    Incident Log
+  </h1>
 
-<h2>incident reports</h2>
-<div>
-<section>incudent</section>
-<section>reporting</section>
-<section>incident from reporting</section>
-</div>
+<main class="flex p-3">
+    <!-- First Section -->
+    <div class="w-1/3 m-3 bg-gray-50 rounded-lg shadow-md">
+      <h2>Incidents</h2>
+    </div>
 
+    <!-- Second Section -->
+    <div class="w-1/3 m-3 bg-gray-50 rounded-lg shadow-md grid">
+      <h2 class="mt-3 ml-3">Incident Report</h2>
 
+      <label class="m-3 grid" for="Description">
+        Description of incident
+        <input
+          type="text"
+          placeholder="Enter Description"
+          class="border rounded-md mt-1 p-1"
+        />
+      </label>
+
+      <label class="m-3 grid" for="Cause">
+        Cause of incident
+        <input type="text" placeholder="Enter Cause"
+        class="border rounded-md mt-1 p-1" />
+      </label>
+
+      <label class="m-3 grid" for="Date">
+        Date of recording
+        <input type="datetime-local" />
+      </label>
+
+      <label class="m-3 grid" for="Photo">
+        Upload a photo
+        
+        {#if avatar}
+        <img src="{avatar}" alt="User Upload" />
+        {:else}
+        <img src="" alt="" />
+        {/if}
+        <button class="bg-gray-50 hover:bg-gray-300 transition border rounded-md m-2 p-1 cursor-pointer" on:click={()=>{fileinput.click();}}>Upload</button>
+        <input style="display:none" type="file" accept=".jpg, .jpeg, .png" on:change={(e)=>onFileSelected(e)} bind:this={fileinput} />
+      </label>
+    </div>
+
+    <!-- Third Section -->
+    <div class="w-1/3 m-3 bg-gray-50 rounded-lg shadow-md">
+      <h2>Locations already logged</h2>
+    </div>
+  
+</main>
 
 <style>
-div{
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    padding-left: 100px;
-}
-h2{
-    
-}
+
 </style>
