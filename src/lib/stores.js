8662b680a9ebe33f0780f5111f6998979da754cf
smitja21 <smitja21@student.op.ac.nz>
2025-04-28 16:01:58 +1200

    fix: added store variable which was removed during merge

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,8 +1,8 @@
 import { writable } from 'svelte/store';
 
 export const selectedAgency = writable('All');
+export const selectedPage = writable('All');
 
 export let hazardAddress= writable('');
-
 export let searchQuery=writable('');
 
