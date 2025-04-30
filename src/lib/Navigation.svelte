3a964e2cd8c9430cf710c2ed3f56c0c4f6d9a8bf
Joshua Smith <169119042+smitja21@users.noreply.github.com>
2025-04-30 22:16:31 +1200

    fix: removed unuseeded selectors and increased background on dropdown

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -7,7 +7,7 @@
     import PageSelect from "$lib/PageSelect.svelte";
     import DarkModeButton from "./DarkModeButton.svelte";
     import LogoutButton from "./LogoutButton.svelte";
-    
+
     function displayMenu() {
         // Toggle the display style of the dropdown menu
         dropdownDisplayed = true;
@@ -32,11 +32,14 @@
 </script>
 
 <header class="flex justify-center items-center sticky top-0 z-[1000] p-2">
-    <h1 class="text-center text-xl uppercase underline tracking-widest p-3 drop-shadow-xl m-4 bg-white w-[25em] shadow-[3px_3px_3px]">
-        <a href="/" class="text-darkgrey no-underline">Disaster Management System</a>
+    <h1
+        class="text-center text-xl uppercase underline tracking-widest p-3 drop-shadow-xl m-4 bg-white w-[25em] shadow-[3px_3px_3px]"
+    >
+        <a href="/" class="text-darkgrey no-underline"
+            >Disaster Management System</a
+        >
     </h1>
     <nav>
-        
         <!--Main Items-->
         <ul class="hidden md:flex space-x-3 text-xl p-4">
             <li class="p-4">
@@ -49,23 +52,27 @@
                 <PageSelect />
             </li>
             <li class="p-4">
-                <LogoutButton/>
+                <LogoutButton />
             </li>
         </ul>
         <!--Hamburger Menu-->
         <ul class="md:hidden ml-8">
             <li>
-                <button id="hamburger" on:click={displayMenu} class="text-4xl cursor-pointer hover:text-gray-800">
+                <button
+                    id="hamburger"
+                    on:click={displayMenu}
+                    class="text-4xl cursor-pointer hover:text-gray-800"
+                >
                     ☰
                 </button>
             </li>
         </ul>
     </nav>
-    <nav id="dropdown-menu" bind:this={dropdownMenu}
-
-        class="hidden absolute top-[10em] w-[20em] h-[12em] bg-[#333] text-white rounded-lg border-4 border-black shadow-[3px_3px_10px_black]">
-
-
+    <nav
+        id="dropdown-menu"
+        bind:this={dropdownMenu}
+        class="hidden absolute top-[10em] w-[20em] h-[12em] bg-[#333] text-white rounded-lg border-4 border-black shadow-[3px_3px_10px_black]"
+    >
         <ul class="flex flex-col space-y-2 p-4">
             <li class="list-disc ml-8">
                 <AgencySelect />
@@ -77,15 +84,14 @@
                 <PageSelect />
             </li>
             <li class="list-disc ml-8">
-                <LogoutButton/>
+                <LogoutButton />
             </li>
         </ul>
     </nav>
-
 </header>
 
 <style>
-    header{
+    header {
         display: flex;
         justify-content: center;
         align-items: center;
@@ -93,7 +99,7 @@
         top: 0;
         z-index: 1000;
     }
-    h1 a{
+    h1 a {
         color: #333;
         text-decoration: none;
     }
@@ -102,20 +108,12 @@
         font-size: 20px;
         text-transform: uppercase;
         text-decoration: underline;
-        letter-spacing: 4px;    
+        letter-spacing: 4px;
         text-align: center;
         background-color: white;
         padding: 0.6em;
         box-shadow: 3px 3px 3px;
         margin-left: 1em;
-    }
-    ul li a {
-        text-decoration: none;
-        color:white;
-        font-weight: 600;
-    }
-    ul li:hover a{    
-        color: #333;  
     }
 
     ul {
@@ -123,60 +121,52 @@
         justify-content: center;
         list-style: none;
         font-size: 1.2em;
-        /* Puts the title onto the same line */      
+        /* Puts the title onto the same line */
         /*Centers list elements directly*/
-        padding: 1em; 
+        padding: 1em;
     }
-    ul li{
+    ul li {
         padding: 0.3em 0.8em 0.3em 0.8em;
         border-radius: 0.625em;
-        margin: 10px;  
+        margin: 10px;
         align-self: center;
     }
     /*ul list that contains the #hamburger a tag ☰*/
-    ul:last-of-type{
+    ul:last-of-type {
         display: none;
     }
 
-    #dropdown-menu{
+    #dropdown-menu {
         display: none;
         background-color: #333;
         width: 20em;
+        height: 20em;
         position: absolute;
         top: 10em;
         border-radius: 1em;
     }
-    #dropdown-menu ul{
+    #dropdown-menu ul {
         flex-direction: column;
     }
-    #dropdown-menu ul li{
+    #dropdown-menu ul li {
         align-self: last baseline;
         list-style: square;
         color: white;
         margin-left: 2em;
     }
-    #dropdown-menu ul li a:hover{
-        background-color: white;
-        color: #333;       
+    #dropdown-menu li {
+        padding: 0;
     }
-  #dropdown-menu a{
-        padding-right: 6em;
-        padding-top: 1em;
-        cursor: pointer;
-    }
- #dropdown-menu li{
-    padding: 0;
- }
-    #hamburger{
+    #hamburger {
         font-size: 40px;
         cursor: pointer;
     }
     ul:has(#hamburger) {
-    /* Styles for the <ul> containing the #hamburger */
+        /* Styles for the <ul> containing the #hamburger */
         margin-left: 2em;
     }
 
-    button{
+    button {
         padding: 0;
         border: none;
         background-color: transparent;
@@ -186,26 +176,22 @@
         color: white;
         cursor: pointer;
     }
-    #button{
-        padding: 0;
-    }
-    #hamburger:hover{
+
+    #hamburger:hover {
         color: #333;
     }
-@media (width <= 1545px){
-    /*First list on nav menu*/
-    ul:first-of-type{
-        display: none;
+    @media (width <= 1545px) {
+        /*First list on nav menu*/
+        ul:first-of-type {
+            display: none;
+        }
+        /*Last list on nav menu for hamburger menu*/
+        ul:last-of-type {
+            display: flex;
+        }
+        #dropdown-menu {
+            border: 5px solid black;
+            box-shadow: 3px 3px 10px black;
+        }
     }
-    /*Last list on nav menu for hamburger menu*/
-    ul:last-of-type{
-        display: flex;
-    }
-    #dropdown-menu{
-        border: 5px solid black;
-        box-shadow: 3px 3px 10px black;
-    }
-}
-
 </style>
-
