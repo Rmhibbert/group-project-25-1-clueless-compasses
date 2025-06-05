3df2df540a374c8eeb5740efdd0fb63ac0a0e7f7
Your Name <smitht8@student.op.ac.nz>
2025-06-05 20:59:10 +1200

    centered text on agency select and page select for consistency

--- src/lib/AgencySelect.svelte
+++ src/lib/AgencySelect.svelte
@@ -5,7 +5,7 @@
 <!-- Dropdown to show specific information -->
 <select
   bind:value={$selectedAgency}
-  class="w-full px-4 py-[0.6em] font-bold bg-[#333] text-white cursor-pointer outline-none border-t-[2px] border-l-[2px] border-b-[2px] border-r-[2px] border-t-[#535353] border-l-[#535353] border-b-black border-r-black"
+  class="w-full text-center px-4 py-[0.6em] font-bold bg-[#333] text-white cursor-pointer outline-none border-t-[2px] border-l-[2px] border-b-[2px] border-r-[2px] border-t-[#535353] border-l-[#535353] border-b-black border-r-black"
 >
   <option value="All">🌍 All</option>
   <option value="FENZ">🚒 FENZ</option>
--- src/lib/PageSelect.svelte
+++ src/lib/PageSelect.svelte
@@ -10,13 +10,15 @@
 </script>
 
 <!-- Binds menu value to a variable, then passes that variable to a function that converts it to a url -->
-<select bind:value={$selectedPage} 
-on:change={PageChange}
-class="w w-full dropdown px-4 py-[0.6em] font-bold bg-[#333] text-white cursor-pointer outline-none border-t-[2px] border-l-[2px] border-b-[2px] border-r-[2px] border-t-[#535353] border-l-[#535353] border-b-black border-r-black"
+<select
+  bind:value={$selectedPage}
+  on:change={PageChange}
+  class="w-full text-center dropdown px-4 py-[0.6em] font-bold bg-[#333] text-white cursor-pointer outline-none border-t-[2px] border-l-[2px] border-b-[2px] border-r-[2px] border-t-[#535353] border-l-[#535353] border-b-black border-r-black"
 >
-    <option value="">Home</option>
-    <option value="hazard">Hazard Reporting</option>
-    <option value="incident">Incident Reporting</option>
+  <option value="">Home</option>
+  <option value="hazard">Hazard Reporting</option>
+  <option value="incident">Incident Reporting</option>
 </select>
+
 
     
